3ecef338c3c532523e7f534dd4ef502c57652c58
Bryl Gorgonio <brylgorgonio@gmail.com>
2025-05-26 22:16:01 +0800

    updated progress.php

--- progress.php
+++ progress.php
@@ -1,4 +1,5 @@
 <?php
 /*
-answered w3schools php exercises until foreach loops
+- answered w3schools php exercises until foreach loops
+- updated login/register forms, can now succesfully register and login but only one account, utilizing session variables
 */
