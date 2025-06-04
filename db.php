bad7ab54063665ce7259ff54345f965ed12923bd
BrylDG <brylgorgonio@gmail.com>
2025-06-04 22:27:56 +0800

    Finished setup for datbase connection

--- db.php
+++ db.php
@@ -4,4 +4,13 @@
     $db_password = "";
     $db_name = "attendance";
     $conn = "";
+
+    $conn = mysqli_connect($db_server, $db_user, $db_password, $db_name);
+
+    if($conn) {
+        echo "Connected to database!";
+    }
+    else {
+        echo "Not connected to database!";
+    }
 ?>
