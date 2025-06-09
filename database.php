05e777ede0ef8479a5d028441ed53e108a96badd
BrylDG <brylgorgonio@gmail.com>
2025-06-09 22:46:55 +0800

    completed departments page

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
     $db_user = "root";
     $db_pass = "";
     $db_name = "attendance";
-    $conn = mysqli_connect($db_server, $db_username, $db_pass, $db_name);
+    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
 
     if ($conn) {
         echo "Connected to database.";
--- departments.php
+++ departments.php
@@ -10,5 +10,37 @@
 </head>
 <body>
     <h1>Departments Management</h1>
+    <a href="addDepartment.php">Add a Department Here</a>
+    <a href="index.php">Back to Menu</a>
+        <?php
+            echo "<table style='border: solid 1px black'>";
+            echo "<tr>";
+            echo "<th>Code</th>";
+            echo "<th>Name</th>";
+            echo "<th>Head</th>";
+            echo "<th>Tel. No.</th>";
+            echo "<th>Actions</th>";
+            echo "</tr>";
+
+            $sql = "SELECT * FROM departments";
+            $result = mysqli_query($conn, $sql);
+            $departments = mysqli_fetch_all($result, MYSQLI_ASSOC);
+            $page = "editDepartments.php";
+            foreach($departments as $department) {
+                echo "<tr>";
+                    echo "<td style='border: solid 1px black'>{$department["depCode"]}</td>";
+                    echo "<td style='border: solid 1px black'>{$department["depName"]}</td>";
+                    echo "<td style='border: solid 1px black'>{$department["depHead"]}</td>";
+                    echo "<td style='border: solid 1px black'>{$department["depTelNo"]}</td>";
+                    echo "<td style='border: solid 1px black'>
+                            <a href={$page}>Edit</a>
+                            <form action='departments.php' method='post'>
+                                <input type='submit' name='delete' value='delete'>
+                            </form>
+                        </td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        ?>
 </body>
 </html>
