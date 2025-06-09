4e9d3ec772fe5d1c4c9af8b073c9d8db89355efd
BrylDG <brylgorgonio@gmail.com>
2025-06-09 22:22:41 +0800

    created new database file

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "attendance";
+    $conn = mysqli_connect($db_server, $db_username, $db_pass, $db_name);
+
+    if ($conn) {
+        echo "Connected to database.";
+    } else {
+        echo "Not Connected to database.";
+    }
+
+?>
